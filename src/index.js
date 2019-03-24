67bb4791af1433296a08cbcd68f64bd87615f690
Saprok123 <schpack.maks@gmail.com>
2019-03-24 23:46:21 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let arr = []
+  let open = []
+  let close = []
+  bracketsConfig.forEach(x => {
+    open.push(x[0])
+    close.push(x[1])
+  })
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i])) {
+      if (close.includes(str[i]) && arr.includes(str[i]))
+        arr.pop()
+      else
+        arr.push(str[i])
+    }
+    
+  }
+  return arr.length == 0
 }
